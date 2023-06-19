def69be534911c0fe10471c4f77b1b5457d9e08b
David Shelton <79729565+DavidSheltonSF@users.noreply.github.com>
2023-06-19 17:29:56 -0300

    Update main.py
    Primeiro Insert

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
 import mysql.connector
+
+conexao = mysql.connector.connect(
+    host ='localhost',
+    user='root',
+    password='1234',
+    database='bdyoutube'
+)
+
+cursor = conexao.cursor()
+
+comando = """INSERT INTO bdyoutube.Vendas
+        (
+            nome_produto,
+            valor
+        )
+        VALUES
+        (
+            'shampoo anticaspa',
+            90.5
+        );
+    """
+
+cursor.execute(comando)
+
+conexao.commit()
+# Fechar cursor e conexão
+cursor.close()
+conexao.close()
